213a83f7e20e059146a93a313e0a5e567fbc4ab1
Kristian <EpicElementz@gmail.com>
2019-01-15 12:37:15 +0100

    Endret html filer til php filer med footer

--- HTML/gruppeEksempel.php
+++ HTML/gruppeEksempel.php
@@ -135,67 +135,9 @@
 			</section>
 
 		</section>
-		</div>
-		<footer>
-			<div class="footer-shortcut-container">
-			<div class="footer-shortcut">
-	          	<h3>Snareveier</h3>
-	            <p><a target="blank" href="https://min.usn.no/student/">Mine grupper</a></p>
-	            <p><a href="logginn.html">Mine profilinstillinger</a></p>
-	            <p><a href="logginn.html">Arrangementer</a></p>
-	            <p><a href="kontakt.html">Labore et dolore</a></p>
-	            <p><a href="gisisamfunnet.html">Lorem ipsum</a></p>
-        	</div>
-
-        	<div class="footer-shortcut">
-	          	<h3>Informasjon</h3>
-	            <p><a href="gisstudiet.html">Om oss</a></p>
-	            <p><a href="gisstudiet.html">Kontakt oss</a></p>
-	            <p><a href="gisstudiet.html">Finn oss</a></p>
-	            <p><a href="faq.html">Ofte stilte spørsmål</a></p>
-        	</div>
-
-        	<div class="footer-shortcut">
-	          	<h3>Støtte</h3>
-	            <p><a href="kontakt.html">Kontakt informasjon</a></p>
-	            <p><a href="kontakt.html">Dolor sit amet</a></p>
-	            <p><a href="kontakt.html">Consectetur adipisicing elit</a></p>
-	            <p><a href="kontakt.html">Labore et dolore</a></p>
-	            <p><a href="gisisamfunnet.html">Lorem ipsum</a></p>
-        	</div>
-
-        	<div class="footer-shortcut">
-	          	<h3>Sosiale medier</h3>
-	          	<a href="#"><img src="https://en.facebookbrand.com/wp-content/uploads/2016/05/flogo_rgb_hex-brc-site-250.png">Facebook</a>
-	          	<a href="#"><img src="https://seeklogo.com/images/T/twitter-2012-negative-logo-5C6C1F1521-seeklogo.com.png">Twitter</a>
-	          	<a href="#"><img src="https://i.kinja-img.com/gawker-media/image/upload/s--OPwjwQZd--/c_scale,f_auto,fl_progressive,q_80,w_800/vsbj7xu992tmklw7hbjj.jpg">Snapchat</a>
-        	</div>
-        	</div>
-
-        	<div class="footer-info">
-	            <p>Sindre Haavaldsen &copy; 2018</p>
-	            <p><a href="gisisamfunnet.html">Opphavsrettsinformasjon</a></p>
-        	</div>
-
-        	<div class="footer-lang">
-        		<div class="lang-selector">
-	            <ul>
-	            	<a href="#">
-	            		<li>
-						<img src="../Pictures/norwegian.svg"/>
-						Norsk
-	            		</li>
-	            	</a>
-	            	<a href="#">
-	            		<li>
-						<img src="../Pictures/english.svg"/>
-						English
-	            		</li>
-	            	</a>
-	            </ul>
-	            </div>
-        	</div>
-		</footer>
+		
+        <?php include '../PHP/footer.php';?>
+        
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
