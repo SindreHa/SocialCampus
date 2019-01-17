b598a40d49e51860e38307eb7091abaf3569c1c8
Kristian <EpicElementZ@gmail.com>
2019-01-17 13:31:12 +0100

    Added template for mysql connection

--- PHP/config.php
+++ PHP/config.php
@@ -1,1 +1,9 @@
-config.php
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'demo');
+ 
+/* Connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
