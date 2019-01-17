c419f62209e37e95ee99bc5c114ad36beeb2a220
Sindre Haavaldsen <sindre.h@outlook.com>
2019-01-17 23:22:35 +0100

    Endre animasjon på logg inn boks
    Også endret fra å bruke opacity: 0 til display: none da opacity gjorde at elementene overlappet andre elementer selv om de var "usynlige"

--- PHP/nav.php
+++ PHP/nav.php
@@ -5,25 +5,25 @@
 		<li><a href="#">Grupper</a></li>
 		<li><a id="click">Logg Inn</a>
 			
-			<div class="log-in hide" id="dropdown">
-			<div class="dropdown-content">
-			<form method="post">
-        	<label>Brukernavn (E-post)</label>
-        		<div class="inputContainer">
-				<i class="fas fa-at input-icon"></i>
-				<input type="text" class="input" name="brukernavn" placeholder="eksempel@epost.no" required>
+			<div class="log-in">
+				<div class="dropdown-content" id="dropdown">
+				<form method="post">
+	        	<label>Brukernavn (E-post)</label>
+	        		<div class="inputContainer">
+					<i class="fas fa-at input-icon"></i>
+					<input type="text" class="input" name="brukernavn" placeholder="eksempel@epost.no" required>
+					</div>
+				<label>Passord</label>
+					<div class="inputContainer" >
+					<i class="fas fa-key input-icon"></i>
+					<input type="password" class="input" name="passord" placeholder="••••••••••" required>
+					</div>
+				<p id="feil">Feil brukernavn eller passord</p>
+				<input type="submit" class="btn" onclick="return checkPsw(form)" value="Logg inn">
+				</form>
+				<p><a href="Registrering.html">Har du ikke bruker?</a></p>
+				<p><a href="Registrering.html">Glemt passord</a></p>
 				</div>
-			<label>Passord</label>
-				<div class="inputContainer" >
-				<i class="fas fa-key input-icon"></i>
-				<input type="password" class="input" name="passord" placeholder="••••••••••" required>
-				</div>
-			<p id="feil">Feil brukernavn eller passord</p>
-			<input type="submit" class="btn" onclick="return checkPsw(form)" value="Logg inn">
-			</form>
-			<p><a href="Registrering.html">Har du ikke bruker?</a></p>
-			<p><a href="Registrering.html">Glemt passord</a></p>
-			</div>
         	</div> 
 		</li>
 	</ul>
