b90b920db9067cf46b4342e506839c7e6e67415a
Torbjørn <torbjoernolaisen@gmail.com>
2019-01-17 13:35:17 +0100

    Endret log-in detaljer

--- PHP/config.php
+++ PHP/config.php
@@ -1,9 +1,11 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'UrqooQQh1D8Hr0ro');
+define('DB_NAME', 'applikasjon');
  
 /* Connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+?>
