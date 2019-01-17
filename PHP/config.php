5598b2668d04a75dd077e81c5a837ffcb4b6df26
Kristian <Epicelementz@gmail.com>
2019-01-17 12:08:06 +0100

    Oprettet logg inn config fil

---
+++ PHP/config.php
@@ -0,0 +1,11 @@
+<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+	<fieldset >
+		<legend>Logg inn</legend>
+		<input type='hidden' name='submitted' id='submitted' value='1'/>
+		<label for='username' >Brukernavn:</label>
+		<input type='text' name='username' id='username'  maxlength="50" />
+		<label for='password' >Passord:</label>
+		<input type='password' name='password' id='password' maxlength="50" />
+		<input type='submit' name='Submit' value='Submit' />
+	</fieldset>
+</form>
