be71fb6b31301d7a7ecab19e7513cd492226ebd5
Kristian <EpicElementz@gmail.com>
2019-01-19 15:50:51 +0100

    Gjort login funksjon kompitabel med database

--- PHP/nav.php
+++ PHP/nav.php
@@ -7,19 +7,19 @@
 			
 			<div class="log-in">
 				<div class="dropdown-content" id="dropdown">
-				<form method="post">
-	        	<label>Brukernavn</label>
+				<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+	        	<label  for='username' >Brukernavn</label>
 	        		<div class="inputContainer">
 					<i class="fas fa-at input-icon"></i>
 					<input type="text" class="input" name="brukernavn" placeholder="eksempel@epost.no" required>
 					</div>
-				<label>Passord</label>
+				<label for='password' >Passord</label>
 					<div class="inputContainer" >
 					<i class="fas fa-key input-icon"></i>
 					<input type="password" class="input" name="passord" placeholder="••••••••••" required>
 					</div>
 				<p id="feil">Feil brukernavn eller passord</p>
-				<input type="submit" class="btn" onclick="return checkPsw(form)" value="Logg inn">
+				<input type="submit" class="btn" onclick="return checkPsw(form)" value="Logg inn" maxlength="40">
 				</form>
 				<p><a href="register.php">Har du ikke bruker?</a></p>
 				<p><a href="Registrering.html">Glemt passord</a></p>
