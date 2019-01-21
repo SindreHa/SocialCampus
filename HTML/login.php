aee87122a80787ceae1ac4eb849282271a7d4e7c
Sindre Haavaldsen <sindre.h@outlook.com>
2019-01-21 19:27:38 +0100

    Rett opp link til config fil

--- HTML/login.php
+++ HTML/login.php
@@ -13,7 +13,7 @@ session_start();
 */
 
 // Include config file
- // require_once "config.php";
+ require_once "../PHP/config.php";
  
 // Define variables and initialize with empty values
 $username = $password = "";
@@ -95,7 +95,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Sign Up</title>
+    <title>Logg inn</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="../CSS/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
@@ -136,7 +136,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     </div>
 
                     <p>Har du ikke en bruker? <a href="register.php">Registrer deg her</a>.</p>
-                    
+
                 </form>
             </section>
         </div>
--- HTML/register.php
+++ HTML/register.php
@@ -1,6 +1,6 @@
 <?php
-// Include config file
-// require_once "/config.php";
+// Bruk Config fil
+require_once "../PHP/config.php";
  
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
