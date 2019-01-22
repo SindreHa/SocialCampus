43cb6090135bb069765f57edceceead512ba296f
Jan Andreas <jan-anan@hotmail.com>
2019-01-22 14:16:12 +0100

    Fjernet unødvendig script-src som lagde error

--- HTML/register.php
+++ HTML/register.php
@@ -154,6 +154,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="../JS/js.js"></script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAHJDa2YV51FDV-MCD1V6JAxU5NqDgTQrc&callback=myMap"></script>
 </body>
 </html>
