a0ba208f5b0b52b9c84b115156f058a2757c30de
Jan Andreas <jan-anan@hotmail.com>
2019-01-23 13:26:47 +0100

    Flytta/forkorta slider kode

--- JS/slider.js
+++ JS/slider.js
@@ -21,74 +21,65 @@ var startLoop = setInterval(function()
 function bildeLoop()
 {
     if(status == 0)
-    byttBilde(0);
+    {
+        bilde[1].style.opacity = "0";
+        setTimeout(function()
+        {
+            bilde[0].style.right = "0px";
+            bilde[1].style.right = -width + "px";
+            bilde[2].style.right = width + "px";
+        }, 500);
 
+        setTimeout(function()
+        {
+            bilde[1].style.opacity = "1";
+        }, 1000);
+
+        status = 1;
+    }
+    
     else if(status == 1)
-    byttBilde(1);
+    {
+        bilde[2].style.opacity = "0";
+        setTimeout(function()
+        {
+            bilde[1].style.right = "0px";
+            bilde[2].style.right = -width + "px";
+            bilde[0].style.right = width + "px";
+        }, 500);
+
+        setTimeout(function()
+        {
+            bilde[2].style.opacity = "1";
+        }, 1000);
+
+        status = 2;
+    }
+    
 
     else if(status == 2)
-    byttBilde(2);
+    {
+        bilde[0].style.opacity = "0";
+        setTimeout(function()
+        {
+            bilde[2].style.right = "0px";
+            bilde[0].style.right = -width + "px";
+            bilde[1].style.right = width + "px";
+        }, 500);
+
+        setTimeout(function()
+        {
+            bilde[0].style.opacity = "1";
+        }, 1000);
+
+        status = 0;
+    }
+
+        
 }
 /* */
 
 
-
-function byttBilde(index){
-
-            if(index == 0)
-            {     
-                bilde[index+1].style.opacity = "0";
-                setTimeout(function()
-                {
-                    bilde[index].style.right = "0px";
-                    bilde[index+1].style.right = -width + "px";
-                    bilde[index+2].style.right = width + "px";
-                }, 500);
-
-                setTimeout(function()
-                {
-                    bilde[index+1].style.opacity = "1";
-                }, 1000);
-            }
-            else if(index == 1)
-            {
-                bilde[index+1].style.opacity = "0";
-                setTimeout(function()
-                {
-                    bilde[index].style.right = "0px";
-                    bilde[index+1].style.right = -width + "px";
-                    bilde[index-1].style.right = width + "px";
-                }, 500);
-
-                setTimeout(function()
-                {
-                    bilde[index+1].style.opacity = "1";
-                }, 1000);
-            }
-            else if(index == 2)
-            {
-                bilde[index-2].style.opacity = "0";
-                setTimeout(function()
-                {
-                    bilde[index].style.right = "0px";
-                    bilde[index-2].style.right = -width + "px";
-                    bilde[index-1].style.right = width + "px";
-                }, 500);
-
-                setTimeout(function()
-                {
-                    bilde[index-2].style.opacity = "1";
-                }, 1000);
-            }
-
-        
-        status = index+1
-        if(status == 3)
-        {
-            status = 0;
-        }
-        
-}
 
 // Slider tar pause fra auto-mode når man holder over
 document.getElementById("slider-container").onmouseenter = function(){
