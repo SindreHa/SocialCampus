c437e2560a3f8160b9756ed66e3e9e170a47bfe1
Jan Andreas <jan-anan@hotmail.com>
2019-01-23 14:00:21 +0100

    Implementert høyre-knapp funksjon for slider
    (Venstre er for komplisert, må ta det etterhvert)

--- JS/slider.js
+++ JS/slider.js
@@ -91,3 +91,88 @@ document.getElementById("slider-container").onmouseleave = function(){
         bildeLoop();
     }, timer);
 }
+
+document.getElementById("knapp-hoyre").onclick = function()
+{
+    bildeLoop();
+}
+
+
+/* --------- Venstre knapp funksjon for slider ------------- */
+
+/*
+document.getElementById("knapp-venstre").onclick = function()
+{
+    if(status == 0)
+        status = 1;
+    
+    else if(status == 1)   
+        status = 2;
+    
+    else if(status == 2)
+        status = 0;   
+        
+        bildeLoopReverse();
+}
+
+
+function bildeLoopReverse()
+{
+    if(status == 0)
+    {
+        bilde[2].style.opacity = "1";
+        setTimeout(function()
+        {
+            bilde[2].style.right = "0px";
+            bilde[1].style.right = -width + "px";
+            bilde[0].style.right = width + "px";
+        }, 500);
+
+        setTimeout(function()
+        {
+            bilde[2].style.opacity = "1";
+        }, 1000);
+
+        status = 1;
+    }
+    
+    else if(status == 1)
+    {
+        bilde[0].style.opacity = "1";
+        setTimeout(function()
+        {
+            bilde[0].style.right = "0px";
+            bilde[2].style.right = -width + "px";
+            bilde[1].style.right = width + "px";
+        }, 500);
+
+        setTimeout(function()
+        {
+            bilde[0].style.opacity = "1";
+        }, 1000);
+
+        status = 2;
+    }
+    
+
+    else if(status == 2)
+    {
+        bilde[1].style.opacity = "1";
+        setTimeout(function()
+        {
+            bilde[1].style.right = "0px";
+            bilde[0].style.right = -width + "px";
+            bilde[2].style.right = width + "px";
+        }, 500);
+
+        setTimeout(function()
+        {
+            bilde[1].style.opacity = "1";
+        }, 1000);
+
+        status = 0;
+    }
+
+        
+}
+*/
