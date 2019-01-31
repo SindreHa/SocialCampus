b4c421ac27735f0d0380de976d3a639770a77fdc
Sindre Haavaldsen <sindre.h@outlook.com>
2019-01-31 16:46:44 +0100

    Flytt vanlige <head> elementer til egen head.php fil

--- HTML/login.php
+++ HTML/login.php
@@ -90,12 +90,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>Logg inn</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600" rel="stylesheet">
+    <?php include '../PHP/head.php';?>
 </head>
 <body>
     <div class="wrapper">
