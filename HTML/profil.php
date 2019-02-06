071b49761a415cd9761c205903a061224bc71a54
Kristian <EpicElementz@gmail.com>
2019-02-06 17:10:23 +0100

    Opprettet php kode for endring av passord

--- HTML/profil.php
+++ HTML/profil.php
@@ -5,7 +5,65 @@ if(!(isset($_SESSION['username'])))
 {
     header("Location: index.php");
 }
-
+// Definer variabler initialiser med tomme verdier
+require_once "../PHP/config.php";
+ 
+// Definer variabler initialiser med tomme verdier
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+ 
+// Definer variabler initialiser med tomme verdier
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Valider nytt passord
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Skriv inn et nytt passord";     
+    } elseif(strlen(trim($_POST["new_password"])) < 8){
+        $new_password_err = "Passord må ha minst 8 karakterer";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+    
+    // Valider confirm passord
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Bekreft ditt passord";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "Passordene er ikke like";
+        }
+    }
+        
+    // Se etter input-error før insetting i database
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        $sql = "UPDATE bruker SET password = ? WHERE id = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+            
+            // Velg parametre 
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+            
+            // Utfør statement
+            if(mysqli_stmt_execute($stmt)){
+                // Passord endret, exit stopp session og gå til profilsiden
+                session_destroy();
+                header("location: profil.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Lukk statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Lukk connection
+    mysqli_close($link);
+}
+?>
 ?>
 
 <!DOCTYPE html>
