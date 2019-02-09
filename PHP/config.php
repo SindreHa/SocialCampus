7b3f1d2320d0c5ea8a682b5bac455a0f5682cf8e
Kristian <EpicElementz@gmail.com>
2019-02-09 12:40:56 +0100

    Endret databasetilkobling til application

--- PHP/config.php
+++ PHP/config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'applikasjon');
+define('DB_NAME', 'application');
  
 /* Connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
