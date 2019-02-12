57b4d0b28d4b7f3e181e3af65eeaada674241426
Kristian <EpicElementz@gmail.com>
2019-02-12 14:13:52 +0100

    Lagt til Switch case med likes/dislikes insert

--- PHP/configLikes.php
+++ PHP/configLikes.php
@@ -8,4 +8,36 @@ define('DB_NAME', 'applikasjon');
 /* Connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-?>
+
+
+// hvis bruker trykker på like eller dislike knapp
+if (isset($_POST['action'])) {
+  $post_id = $_POST['post_id'];
+  $action = $_POST['action'];
+  switch ($action) {
+  	case 'like':
+         $sql="INSERT INTO likes (user_id, post_id, content) 
+         	   VALUES ($user_id, $post_id, 'like') 
+         	   ON DUPLICATE KEY UPDATE content='like'";
+         break;
+  	case 'dislike':
+          $sql="INSERT INTO likes (user_id, post_id, content) 
+               VALUES ($user_id, $post_id, 'dislike') 
+         	   ON DUPLICATE KEY UPDATE content='dislike'";
+         break;
+  	case 'unlike':
+	      $sql="DELETE FROM likes WHERE user_id=$user_id AND post_id=$post_id";
+	      break;
+  	case 'undislike':
+      	  $sql="DELETE FROM likes WHERE user_id=$user_id AND post_id=$post_id";
+      break;
+  	default:
+  		break;
+  }
+
+  // execute query to effect changes in the database ...
+  mysqli_query($conn, $sql);
+  echo getRating($post_id);
+  exit(0);
+}
+
