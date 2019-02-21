12471c3a2f534ea168ae7d9a7683a1b58e8c0fee
Kristian <EpicElementZ@gmail.com>
2019-02-21 12:34:08 +0100

    Forenklet profilbilde php kode

--- PHP/profilePhoto.php
+++ PHP/profilePhoto.php
@@ -1,53 +1,28 @@
 <?php
-$user="Hei12345"; 
-$pull="select * from user WHERE username='$user'";
-$allowedExts = array("jpg", "jpeg", "gif", "png","JPG");
-$extension = @end(explode(".", $_FILES["file"]["name"]));
-
-if(isset($_POST['pupload'])){
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/JPG")
-|| ($_FILES["file"]["type"] == "image/png")
-|| ($_FILES["file"]["type"] == "image/pjpeg"))
-&& ($_FILES["file"]["size"] < 10000000)
-&& in_array($extension, $allowedExts))
-
-    {
- if ($_FILES["file"]["error"] > 0)
- {
- echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
- }
- else
- {
- echo '<div class="plus">';
- echo "Opplastning vellykket";
- echo '</div>';echo"<br/><b><u>Bilde Detaljer></b><br/>";
- echo "Name: " . $_FILES["file"]["name"] . "<br/>";
- echo "Type: " . $_FILES["file"]["type"] . "<br/>";
- echo "Size: " . ceil(($_FILES["file"]["size"] / 1024)) . " KB";
-     
-if (file_exists("upload/" . $_FILES["file"]["name"]))
- {
- unlink("upload/" . $_FILES["file"]["name"]);
- }
- else{
- $pic=$_FILES["file"]["name"];
- $conv=explode(".",$pic);
- $ext=$conv['1'];
- move_uploaded_file($_FILES["file"]["tmp_name"],"upload/". $user.".".$ext);
- echo "Stored in as: " . "upload/".$user.".".$ext;
- $url=$user.".".$ext;
- $query="update profilePhoto set url='$url', lastUpload=now() where user='$user'";
- if($upl=$db->query($query)){
-echo "<br/>Lagret i database";
- }
- }
- }
-}else{
- echo "Filen din er større enn 10MB, prøv igjen";
+if(!empty($_FILES['picture']['name'])){
+    //Inkluder config fil for databasen
+    include_once 'config.php';
+    
+    //Konfigurer filopplastning
+    $result = 0;
+    $uploadDir = "uploads/images/";
+    $fileName = time().'_'.basename($_FILES['picture']['name']);
+    $targetPath = $uploadDir. $fileName;
+    
+    //Last opp fil til server
+    if(@move_uploaded_file($_FILES['picture']['tmp_name'], $targetPath)){
+        //Get current user ID from session
+        $userId = 1;
+        
+        //Oppdater bildenavn i databasen
+        $update = $db->query("UPDATE user SET avatar = '".$fileName."' WHERE id = $userId");
+        
+        //Oppdater status
+        if($update){
+            $result = 1;
+        }
+    }
+    
+    //JavaScript function for å vise opplastningstatus
+    echo '<script type="text/javascript">window.top.window.completeUpload(' . $result . ',\'' . $targetPath . '\');</script>  ';
 }
-}
-?>     
-     
- 
