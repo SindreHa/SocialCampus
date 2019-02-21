8dc1bb97e79a54660e0531317437b95d57304c8d
Kristian <EpicElementZ@gmail.com>
2019-02-21 12:08:48 +0100

    Lagt til bilde detaljer for profilePhoto

--- PHP/profilePhoto.php
+++ PHP/profilePhoto.php
@@ -12,3 +12,21 @@ if ((($_FILES["file"]["type"] == "image/gif")
 || ($_FILES["file"]["type"] == "image/pjpeg"))
 && ($_FILES["file"]["size"] < 10000000)
 && in_array($extension, $allowedExts))
+
+    {
+ if ($_FILES["file"]["error"] > 0)
+ {
+ echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+ }
+ else
+ {
+ echo '<div class="plus">';
+ echo "Opplastning vellykket";
+ echo '</div>';echo"<br/><b><u>Bilde Detaljer></b><br/>";
+ echo "Name: " . $_FILES["file"]["name"] . "<br/>";
+ echo "Type: " . $_FILES["file"]["type"] . "<br/>";
+ echo "Size: " . ceil(($_FILES["file"]["size"] / 1024)) . " KB";
+     
+     
+     
+ 
