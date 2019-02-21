eaa59a2f0362beb5787ff6b1760f29691906d0c4
Kristian <EpicElementZ@gmail.com>
2019-02-21 13:33:00 +0100

    Endret userId til å være brukerens sessionID

--- PHP/profilePhoto.php
+++ PHP/profilePhoto.php
@@ -1,18 +1,19 @@
 <?php
-if(!empty($_FILES['picture']['name'])){
+if(!empty($_FILES['avatar']['name'])){
     //Inkluder config fil for databasen
     include_once 'config.php';
     
     //Konfigurer filopplastning
     $result = 0;
     $uploadDir = "../Pictures/upload/";
-    $fileName = time().'_'.basename($_FILES['picture']['name']);
+    $fileName = time().'_'.basename($_FILES['avatar']['name']);
     $targetPath = $uploadDir. $fileName;
+    $userId = $_SESSION['id'];
     
     //Last opp fil til server
-    if(@move_uploaded_file($_FILES['picture']['tmp_name'], $targetPath)){
-        //Get current user ID from session
-        $userId = 1;
+    if(@move_uploaded_file($_FILES['avatar']['tmp_name'], $targetPath)){
+        
+        
         
         //Oppdater bildenavn i databasen
         $update = $db->query("UPDATE user SET avatar = '".$fileName."' WHERE id = $userId");
