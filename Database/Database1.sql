e9baac7b92562befbbabdb970ddf3aedde2aa820
Torbjørn <torbjoernolaisen@gmail.com>
2019-02-28 14:23:50 +0100

    Ny database uten Status og User_status

--- Database/Database1.sql
+++ Database/Database1.sql
@@ -1,4 +1,4 @@
--- MySQL Workbench 
+-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
@@ -14,18 +14,29 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema application
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `application` DEFAULT CHARACTER SET utf8  ;
+CREATE SCHEMA IF NOT EXISTS `application` DEFAULT CHARACTER SET latin1 ;
 USE `application` ;
 
 -- -----------------------------------------------------
--- Table `application`.`user_status`
+-- Table `application`.`category`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `application`.`user_status` ;
+DROP TABLE IF EXISTS `application`.`category` ;
 
-CREATE TABLE IF NOT EXISTS `application`.`user_status` (
+CREATE TABLE IF NOT EXISTS `application`.`category` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`))
+  `username` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  `creator` INT(11) NOT NULL,
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status_id` INT(11) NOT NULL,
+  `user_status_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `creator_idx` (`creator` ASC),
+  CONSTRAINT `category_foreign_key3`
+    FOREIGN KEY (`creator`)
+    REFERENCES `application`.`category` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -46,60 +57,53 @@ CREATE TABLE IF NOT EXISTS `application`.`user` (
   `moderator` TINYINT(1) NOT NULL,
   `last_activity` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user_status_id` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `user_status_id` (`user_status_id` ASC),
-  CONSTRAINT `user_foreign_key`
-    FOREIGN KEY (`user_status_id`)
-    REFERENCES `application`.`user_status` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `application`.`status`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `application`.`status` ;
-
-CREATE TABLE IF NOT EXISTS `application`.`status` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `application`.`category`
+-- Table `application`.`post`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `application`.`category` ;
+DROP TABLE IF EXISTS `application`.`post` ;
 
-CREATE TABLE IF NOT EXISTS `application`.`category` (
+CREATE TABLE IF NOT EXISTS `application`.`post` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  `creator` INT(11) NOT NULL,
+  `content` VARCHAR(100) NOT NULL,
   `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `status_id` INT(11) NOT NULL,
-  `user_status_id` INT(11) NOT NULL,
+  `user_id` INT(11) NULL DEFAULT NULL,
+  `status_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `creator_idx` (`creator` ASC),
-  INDEX `fk_category_status1_idx` (`status_id` ASC),
-  INDEX `fk_category_user_status1_idx` (`user_status_id` ASC),
-  CONSTRAINT `category_foreign_key1`
-    FOREIGN KEY (`user_status_id`)
-    REFERENCES `application`.`user_status` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `category_foreign_key2`
-    FOREIGN KEY (`status_id`)
-    REFERENCES `application`.`status` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `category_foreign_key3`
-    FOREIGN KEY (`creator`)
-    REFERENCES `application`.`category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  INDEX `user_id_idx` (`user_id` ASC),
+  CONSTRAINT `post_foreign_key1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `application`.`user` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `application`.`commentary`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `application`.`commentary` ;
+
+CREATE TABLE IF NOT EXISTS `application`.`commentary` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `content` VARCHAR(100) NOT NULL,
+  `made` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `post_id` INT(11) NULL DEFAULT NULL,
+  `user_id` INT(11) NULL DEFAULT NULL,
+  `status_id` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `post_id` (`post_id` ASC),
+  INDEX `user_id` (`user_id` ASC),
+  CONSTRAINT `commentary_foreign_key1`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `application`.`post` (`id`),
+  CONSTRAINT `commentary_foreign_key2`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `application`.`user` (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -150,25 +154,29 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `application`.`post`
+-- Table `application`.`likes`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `application`.`post` ;
+DROP TABLE IF EXISTS `application`.`likes` ;
 
-CREATE TABLE IF NOT EXISTS `application`.`post` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `content` VARCHAR(100) NOT NULL,
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_id` INT(11) NULL DEFAULT NULL,
-  `status_id` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `user_id_idx` (`user_id` ASC),
-  INDEX `status_id_idx` (`status_id` ASC),
-  CONSTRAINT `post_foreign_key1`
+CREATE TABLE IF NOT EXISTS `application`.`likes` (
+  `user_id` INT(11) NOT NULL,
+  `post_id` INT(11) NOT NULL,
+  `rating` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`user_id`, `post_id`),
+  UNIQUE INDEX `UC_rating_info` (`user_id` ASC, `post_id` ASC),
+  UNIQUE INDEX `user_user_info` (`user_id` ASC, `post_id` ASC),
+  INDEX `fk_user_has_post_idx1` (`post_id` ASC),
+  INDEX `fk_user_has_post_idx2` (`user_id` ASC),
+  CONSTRAINT `likes_foreign_key1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `application`.`user` (`id`),
-  CONSTRAINT `post_foreign_key2`
-    FOREIGN KEY (`status_id`)
-    REFERENCES `application`.`status` (`id`))
+    REFERENCES `application`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `likes_foreign_key2`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `application`.`post` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -188,47 +196,62 @@ CREATE TABLE IF NOT EXISTS `application`.`thread` (
   PRIMARY KEY (`id`),
   INDEX `post_id` (`post_id` ASC),
   INDEX `user_id` (`user_id` ASC),
-  INDEX `status_id` (`status_id` ASC),
   CONSTRAINT `thread_foreign_key1`
     FOREIGN KEY (`post_id`)
     REFERENCES `application`.`post` (`id`),
   CONSTRAINT `thread_foreign_key2`
     FOREIGN KEY (`user_id`)
-    REFERENCES `application`.`user` (`id`),
-  CONSTRAINT `thread_foreign_key3`
-    FOREIGN KEY (`status_id`)
-    REFERENCES `application`.`status` (`id`))
+    REFERENCES `application`.`user` (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+USE `application` ;
 
 -- -----------------------------------------------------
--- Table `application`.`likes`
+-- procedure newUser
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `application`.`likes` ;
 
-CREATE TABLE IF NOT EXISTS `application`.`likes` (
-  `user_id` INT(11) NOT NULL,
-  `post_id` INT(11) NOT NULL,
-  `rating` VARCHAR(45) NOT NULL,
-CONSTRAINT UC_rating_info UNIQUE (user_id, post_id),
-  PRIMARY KEY (`user_id`, `post_id`),
-  UNIQUE INDEX `user_user_info` (`user_id` ASC, `post_id` ASC),
-  INDEX `fk_user_has_post_idx1` (`post_id` ASC),
-  INDEX `fk_user_has_post_idx2` (`user_id` ASC),
-  CONSTRAINT `likes_foreign_key1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `application`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `likes_foreign_key2`
-    FOREIGN KEY (`post_id`)
-    REFERENCES `application`.`post` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+USE `application`;
+DROP procedure IF EXISTS `application`.`newUser`;
 
+DELIMITER $$
+USE `application`$$
+CREATE DEFINER=`root`@`127.0.0.1` PROCEDURE `newUser`(
+	in p_username varchar(50),
+    in p_password varchar(255),
+    in p_email varchar(245)
+)
+begin
+
+    insert into user (username, password, email)
+    values (p_username, p_password, p_email);
+end$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure updateProfil
+-- -----------------------------------------------------
+
+USE `application`;
+DROP procedure IF EXISTS `application`.`updateProfil`;
+
+DELIMITER $$
+USE `application`$$
+CREATE DEFINER=`root`@`127.0.0.1` PROCEDURE `updateProfil`(
+	in p_id int(11),
+	in p_full_name varchar(255),
+    in p_picture blob
+)
+begin
+	update user
+	set
+		full_name = p_full_name,
+        picture = p_picture
+	where id = p_id;
+end$$
+
+DELIMITER ;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
