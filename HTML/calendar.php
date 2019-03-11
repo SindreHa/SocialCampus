790a884e670168f6565792a76baea8df831eb3f7
Kristian <EpicElementZ@gmail.com>
2019-03-11 16:47:10 +0000

    Opprettet kalender fil

---
+++ HTML/calendar.php
@@ -0,0 +1,11 @@
+<?php 
+
+/* tegner en kalender */
+
+function draw_calendar($month,$year){
+
+	/* tegne tabell */
+    
+	$calendar = '<table cellpadding="0" cellspacing="0" class="calendar">';
+
+    ;?>
