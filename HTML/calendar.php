4693123889f272daf8721e28e594171191971c4b
Kristian <EpicElementZ@gmail.com>
2019-03-11 16:54:45 +0000

    Lagt til dager og uker

--- HTML/calendar.php
+++ HTML/calendar.php
@@ -8,4 +8,22 @@ function draw_calendar($month,$year){
     
 	$calendar = '<table cellpadding="0" cellspacing="0" class="calendar">';
 
+    
+    
+    /* tabell header */
+	$header = array('Søndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','Lørdag');
+    
+	$calendar.= '<tr class= "calendar-row"> <td class="calendar-day-head">'
+    .implode('</td><td class="calendar-day-head">',$header).'</td></tr>';
+    
+    
+    /* dager og uker */
+	$running_day = date('w',mktime(0,0,0,$month,1,$year));
+	$month_days = date('t',mktime(0,0,0,$month,1,$year));
+	$week_days = 1;
+	$day_count = 0;
+	$date_array = array();
+    $calendar.= '<tr class="calendar-row">';
+    
+    
     ;?>
