7e752c4cbb2660a9dcb5bee799cdd55e90fcba4c
Kristian <EpicElementZ@gmail.com>
2019-03-11 17:24:35 +0000

    Lagt til Kalender knapper

--- HTML/calendar.php
+++ HTML/calendar.php
@@ -1,10 +1,42 @@
 <!DOCTYPE html>
 <html lang="no">
+
 <head>
-	<title>Kalender</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="css/calendar.css">
+    <title>Kalender</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/calendar.css">
 </head>
+
 <body>
+    <!-- Kalender knapper -->
+    <div class="container">
+        <div class="page-header">
+            <div class="pull-right form-inline">
+                <div class="btn-group">
+                    <button class="btn btn-primary" data-calendar-nav="prev"> Forrige </button>
+                    <button class="btn btn-default" data-calendar-nav="today">i dag</button>
+                    <button class="btn btn-primary" data-calendar-nav="next">Neste >></button>
+                </div>
+                <div class="btn-group">
+                    <button class="btn btn-warning" data-calendar-view="year">År</button>
+                    <button class="btn btn-warning active" data-calendar-view="month">Måned</button>
+                    <button class="btn btn-warning" data-calendar-view="week">Uke</button>
+                    <button class="btn btn-warning" data-calendar-view="day">Dag</button>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-9">
+                <div id="showEventCalendar"></div>
+            </div>
+            <div class="col-md-3">
+                <h4>Alle events</h4>
+                <ul id="eventlist" class="nav nav-list"></ul>
+            </div>
+        </div>
+    </div>
+
+
