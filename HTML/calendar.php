6cf5c0a4391746d1f2e59a5632c45acddef1ddb3
Kristian <EpicElementZ@gmail.com>
2019-03-11 17:14:49 +0000

    Inkluderer plugin for bootstrap kalender

--- HTML/calendar.php
+++ HTML/calendar.php
@@ -2,5 +2,9 @@
 <html lang="no">
 <head>
 	<title>Kalender</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/calendar.css">
 </head>
 <body>
