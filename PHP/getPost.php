bea394bb45ea8aaaa7f08a269ddaedba3cb59a32
Sindre Haavaldsen <sindre.h@outlook.com>
2019-03-14 16:58:13 +0100

    Faste kommentarer på innlegg som eksempel

--- PHP/getPost.php
+++ PHP/getPost.php
@@ -58,7 +58,7 @@ if(mysqli_num_rows($result) > 0)
 						<i class="fas fa-caret-down"></i>
 					</a>
 				</div>
-			<h4><a class="comment-collapse" href="#/">0 kommentarer</a></h4> <!-- Antall kommentarer -->
+			<h4><a class="comment-collapse" href="#/">2 kommentarer</a></h4> <!-- Antall kommentarer -->
 			<h4>Publisert</h4>
 			<h4><?php echo $row[3]; ?></h4> <!-- PHP DATO -->
 		</div>
@@ -66,6 +66,50 @@ if(mysqli_num_rows($result) > 0)
 
 	<div class="comment-container">
 			<div class="comment-toggle"> <!-- Kommentarfelt innhold -->
+
+			<div class="post-comment">
+				<div class="user-container-comment">
+					<div class="imgContainer">
+						<img src="../Pictures/kristian.jpg">
+					</div>
+					<div class="comment-vote">
+						<a href="#/" onclick="IncrementPostLikes(this)">
+							<i class="fas fa-caret-up"></i>
+						</a>
+						<p class="ant-likes">2</p>
+						<a href="#/" onclick="DecrementPostLikes(this)">
+							<i class="fas fa-caret-down"></i>
+						</a>
+					</div>
+				</div>
+				<div class="comment-content">
+					<h3>Kristian</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore magnam dolor hic harum? Facilis possimus, tenetur aperiam facere molestiae eaque, quis soluta perferendis blanditiis mollitia eos, illum odio deserunt maiores? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium dignissimos </p>
+					<p>10.12.19 16:34:23</p>
+				</div>
+			</div>
+			
+			<div class="post-comment">
+				<div class="user-container-comment">
+					<div class="imgContainer">
+						<img src="../Pictures/torbjorn.jpg">
+					</div>
+					<div class="comment-vote">
+						<a href="#/" onclick="IncrementPostLikes(this)">
+							<i class="fas fa-caret-up"></i>
+						</a>
+						<p class="ant-likes">4</p>
+						<a href="#/" onclick="DecrementPostLikes(this)">
+							<i class="fas fa-caret-down"></i>
+						</a>
+					</div>
+				</div>
+				<div class="comment-content">
+					<h3>Torbjørb</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore magnam dolor hic harum? Facilis possimus, tenetur aperiam facere molestiae eaque, quis soluta perferendis blanditiis mollitia eos, illum odio deserunt maiores? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium dignissimos. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam vel assumenda distinctio at voluptas excepturi. Sapiente quam explicabo nesciunt veniam, doloremque fuga officia enim ipsa asperiores esse sed natus fugit? </p>
+					<p>10.12.19 16:34:23</p>
+				</div>
+			</div>
 
 			</div>
 			<div class="comment-submit-container"> <!-- Kommentarfelt publisering -->
@@ -79,7 +123,7 @@ if(mysqli_num_rows($result) > 0)
 					}?>
 					</div>
 				</div>
-				<form>
+				<form action="<?php echo htmlspecialchars($_SERVER["REQUEST_URI"]); ?>"class="comment-input" id="comment-form" method="post">
 				<div class="inputContainer">
 					<input type="text" class="input" name="kommentar" placeholder="Skriv en kommentar" autocomplete="off">
 					<input type="submit" style="position: absolute; left: -9999px; width: 1px; height: 1px;"tabindex="-1">
@@ -90,13 +134,6 @@ if(mysqli_num_rows($result) > 0)
 
 </div>
 
-<script>
-	$(".comment-collapse").click( function(e) {
-			e.preventDefault(); 
-			$(this).closest(".post-container").next().find(".comment-toggle").slideToggle();
-			return false; 
-		});
-</script>
 <?php
 
     }
