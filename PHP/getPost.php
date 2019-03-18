a097ef3adb964359f1757bef4b21e6955bbf9ef3
Sindre Haavaldsen <sindre.h@outlook.com>
2019-03-18 20:34:13 +0100

    Hent inn brukernavn for poster

--- PHP/getPost.php
+++ PHP/getPost.php
@@ -1,12 +1,11 @@
-
 <?php
 
 require_once "../PHP/config.php";
 
-$_SESSION['count'] = 4;
-$antPoster = $_SESSION['count'];
+// $_SESSION['count'] = 4;
+// $antPoster = $_SESSION['count'];
 
-$sql = "SELECT * FROM post ORDER BY id DESC LIMIT $antPoster";
+$sql = "SELECT p.id, u.id, p.title, p.content, p.created, u.username FROM application.post AS p, application.user AS u WHERE p.user_id = u.id ORDER BY p.id DESC	;";
 $result = mysqli_query($link, $sql);
 
 session_start();
@@ -33,21 +32,21 @@ if(mysqli_num_rows($result) > 0)
 	<div class="user-container">
 		<div class="user-container-comment">
 			<div class="imgContainer">
-				<?php if(empty($row[4])) 
+				<?php if(empty($row[1])) 
 				{?>
 					<img src="../Pictures/upload/placeholder-profile.png">
 				<?php } 
 				else {?> 
-					<img src="../Pictures/upload/USER_<?php echo $row[4]; ?> ProfilePhoto.png"> <!-- Bilde av poster --> <?php 
+					<img src="../Pictures/upload/USER_<?php echo $row[1]; ?> ProfilePhoto.png"> <!-- Bilde av poster --> <?php 
 				}?>
 			</div>
 		</div>
-		<h4>Brukernavn</h4> <!-- Brukernavn for innlegg -->
+		<h4><?php echo $row[5]; ?></h4> <!-- Brukernavn for innlegg -->
 	</div>
 
 	<div class="post-container">
-		<h2><?php echo $row[1]; ?></h2> <!-- Tittel på innlegg -->
-		<p><?php echo $row[2]; ?> </p> <!-- Innlegg innhold -->
+		<h2><?php echo $row[2]; ?></h2> <!-- Tittel på innlegg -->
+		<p><?php echo $row[3]; ?> </p> <!-- Innlegg innhold -->
 		<div class="post-stats">
 				<div>
 					<a href="#/" class="like-button" onclick="IncrementPostLikes(this)">
@@ -60,7 +59,7 @@ if(mysqli_num_rows($result) > 0)
 				</div>
 			<h4><a class="comment-collapse" href="#/">2 kommentarer</a></h4> <!-- Antall kommentarer -->
 			<h4>Publisert</h4>
-			<h4><?php echo $row[3]; ?></h4> <!-- PHP DATO -->
+			<h4><?php echo $row[4]; ?></h4> <!-- PHP DATO -->
 		</div>
 	</div>
 
@@ -105,7 +104,7 @@ if(mysqli_num_rows($result) > 0)
 					</div>
 				</div>
 				<div class="comment-content">
-					<h3>Torbjørb</h3>
+					<h3>Torbjørn</h3>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore magnam dolor hic harum? Facilis possimus, tenetur aperiam facere molestiae eaque, quis soluta perferendis blanditiis mollitia eos, illum odio deserunt maiores? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium dignissimos. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam vel assumenda distinctio at voluptas excepturi. Sapiente quam explicabo nesciunt veniam, doloremque fuga officia enim ipsa asperiores esse sed natus fugit? </p>
 					<p>10.12.19 16:34:23</p>
 				</div>
