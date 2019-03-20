4116af26fc8a6db76ab2d71744f2778a219f3efe
Sindre Haavaldsen <sindre.h@outlook.com>
2019-03-20 15:02:02 +0100

    Revert "Gjør om class til id på tooltip"
    This reverts commit 248b4435ae727fa5c39394298cf9556df98651e7.

--- PHP/footer.php
+++ PHP/footer.php
@@ -61,7 +61,7 @@
   <i class="fas fa-arrow-up"></i>
 </div>
 
-<div id="tooltip">
+<div class="tooltip" id="notification-box">
   <a href="#/"><i class="fas fa-times"></i></a>
   <i class="fas fa-info-circle"></i><p>Lorem ipsum dolor sit, amet</p>
 </div>
