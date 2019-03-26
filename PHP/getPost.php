efac42d1a65c1d19e8babd0cc84635f5532b04b0
Sindre Haavaldsen <sindre.h@outlook.com>
2019-03-26 16:59:36 +0100

    + på forrige commit

--- PHP/getPost.php
+++ PHP/getPost.php
@@ -107,10 +107,22 @@ if(mysqli_num_rows($resultPost) > 0)
 						<div class="imgContainer">
 							<img src="../Pictures/upload/USER_<?php echo $rowCom[4]; ?> ProfilePhoto.png">
 						</div>
-					<h3><?php echo $rowCom[5]; ?></h3>
 					</div>
-					<p><?php echo $rowCom[1]; ?></p>
-					<p><?php echo $rowCom[2]; ?></p>
+					<div class="text-container">
+						<h3><?php echo $rowCom[5]; ?></h3> <!-- Brukernavn -->
+						<p><?php echo $rowCom[1]; ?></p> <!-- Kommentar innhold -->
+						<div class="comment-stats">
+							<p><?php echo $rowCom[2]; ?></p>
+							<div class="like-button">
+							<form method="post" action="">
+								<a href="#/" onclick="IncrementPostLikes(this)">
+								<i class="fas fa-thumbs-up"></i>
+									<p class="ant-likes">0</p> <!-- Antall likes -->
+								</a>
+							</form>
+							</div>
+						</div> <!-- Kommentar dato -->
+					</div>
 				</div>
 			</div>
 			<?php
