d79a631a59cd04eb8c68c9cd9cf758885ca9e461
Sindre Haavaldsen <sindre.h@outlook.com>
2019-04-06 14:19:27 +0200

    Flyttet box div'er inn i <a> tag

--- HTML/index.php
+++ HTML/index.php
@@ -50,46 +50,46 @@ include '../PHP/nav.php';
 			</form>
 	</section>
 	<section class="boxes">
-		<a href="#"><div class="box">
+		<a href="#" class="box">
 			<i class="fas fa-table-tennis fa-4x"></i>
 			<h3>Bordtennis</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-		</div></a>
-		<a href="#"><div class="box">
+		</a>
+		<a href="#" class="box">
 			<i class="fas fa-futbol fa-4x"></i>
 			<h3>Fotball</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-		</div></a>
-		<a href="#"><div class="box">
+		</a>
+		<a href="#" class="box">
 			<i class="fas fa-volleyball-ball fa-4x"></i>
 			<h3>Volleyball</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-		</div></a>
-		<a href="#"><div class="box">
+		</a>
+		<a href="#" class="box">
 			<i class="fas fa-basketball-ball fa-4x"></i>
 			<h3>Basketball</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-		</div></a>
-		<a href="gruppeEksempel.php"><div class="box">
+		</a>
+		<a href="gruppeEksempel.php" class="box">
 			<i class="fas fa-golf-ball fa-4x"></i>
 			<h3>Golf</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-		</div></a>
-		<a href="#"><div class="box">
+		</a>
+		<a href="#" class="box">
 			<i class="fas fa-gamepad fa-4x"></i>
 			<h3>Spilling</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-		</div></a>
-		<a href="#"><div class="box">
+		</a>
+		<a href="#" class="box">
 			<i class="fas fa-camera fa-4x"></i>
 			<h3>Fotografering</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-		</div></a>
-		<a href="#"><div class="box">
+		</a>
+		<a href="#" class="box">
 			<i class="fas fa-book fa-4x"></i>
 			<h3>Bokklubb</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-		</div></a>
+		</a>
 	</section>
 </div>
         
