0b7149bda4e6e9d0414b9434969027b3ff2e2ebf
Sindre Haavaldsen <sindre.h@outlook.com>
2019-04-08 14:12:01 +0200

    Rettet opp i database script

--- Database/Database.sql
+++ Database/Database.sql
@@ -7,6 +7,9 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema application
+-- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema application
@@ -14,28 +17,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 DROP SCHEMA IF EXISTS `application`;
 CREATE SCHEMA IF NOT EXISTS `application` DEFAULT CHARACTER SET utf8 ;
 USE `application` ;
-
--- -----------------------------------------------------
--- Table `application`.`category`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `application`.`category` ;
-
-CREATE TABLE IF NOT EXISTS `application`.`category` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  `creator` INT(11) NOT NULL,
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  INDEX `creator_idx` (`creator` ASC),
-  CONSTRAINT `category_foreign_key3`
-    FOREIGN KEY (`creator`)
-    REFERENCES `application`.`category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
 
 -- -----------------------------------------------------
 -- Table `application`.`user`
@@ -51,9 +32,25 @@ CREATE TABLE IF NOT EXISTS `application`.`user` (
   `email` VARCHAR(245) NOT NULL,
   `avatar` VARCHAR(100) NOT NULL,
   `moderator` TINYINT(1) NOT NULL,
+  UNIQUE INDEX username_unique (username),
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `application`.`groups`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `application`.`groups` ;
+
+CREATE TABLE IF NOT EXISTS `application`.`groups` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  UNIQUE INDEX groups_name_unique(`name`),
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -66,14 +63,14 @@ CREATE TABLE IF NOT EXISTS `application`.`post` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
   `content` VARCHAR(855) NOT NULL,
-  `likes` INT (11) NOT NULL DEFAULT '0',
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `likes` INT(11) NULL DEFAULT '0',
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `user_id_idx` (`user_id` ASC),
-  CONSTRAINT `post_foreign_key1`
+  CONSTRAINT `fk_post_user`
     FOREIGN KEY (`user_id`)
-    REFERENCES `application`.`user` (`id`))
+    REFERENCES `user` (`id`)
+)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -86,7 +83,8 @@ DROP TABLE IF EXISTS `application`.`commentary` ;
 CREATE TABLE IF NOT EXISTS `application`.`commentary` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `content` VARCHAR(850) NOT NULL,
-  `made` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `made` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `likes` INT(11) NOT NULL DEFAULT '0',
   `post_id` INT(11) NULL DEFAULT NULL,
   `user_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -98,26 +96,6 @@ CREATE TABLE IF NOT EXISTS `application`.`commentary` (
   CONSTRAINT `commentary_foreign_key2`
     FOREIGN KEY (`user_id`)
     REFERENCES `application`.`user` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `application`.`groups`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `application`.`groups` ;
-
-CREATE TABLE IF NOT EXISTS `application`.`groups` (
-  `id` INT(11) NOT NULL,
-  `username` VARCHAR(45) NOT NULL,
-  `category_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_group_category1_idx` (`category_id` ASC),
-  CONSTRAINT `groups_foreign_key`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `application`.`category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -135,14 +113,11 @@ CREATE TABLE IF NOT EXISTS `application`.`groups_has_users` (
   INDEX `fk_groups_has_users2_idx` (`groups_id` ASC),
   CONSTRAINT `groups_has_users_foreign_key1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `application`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `application`.`user` (`id`),
   CONSTRAINT `groups_has_users_foreign_key2`
     FOREIGN KEY (`groups_id`)
     REFERENCES `application`.`groups` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -153,97 +128,27 @@ DEFAULT CHARACTER SET = utf8;
 DROP TABLE IF EXISTS `application`.`likes` ;
 
 CREATE TABLE IF NOT EXISTS `application`.`likes` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `user_id` INT(11) NOT NULL,
   `post_id` INT(11) NOT NULL,
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `commentary_id` INT(11) NULL,
+  `like_unlike` INT(2) NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `likes_foreign_key1`
+  INDEX `likes_foreign_key1` (`user_id` ASC),
+  INDEX `likes_foreign_key2` (`post_id` ASC),
+  CONSTRAINT `user_foreign_key`
     FOREIGN KEY (`user_id`)
-    REFERENCES `application`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `likes_foreign_key2`
+    REFERENCES `application`.`user` (`id`),
+  CONSTRAINT `post_foreign_key`
     FOREIGN KEY (`post_id`)
-    REFERENCES `application`.`post` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    REFERENCES `application`.`post` (`id`),
+  CONSTRAINT `commentary_foreign_key`
+    FOREIGN KEY (`commentary_id`)
+    REFERENCES `application`.`commentary` (`id`)
+)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
--- -----------------------------------------------------
--- procedure newCat
--- -----------------------------------------------------
-DROP procedure IF EXISTS `application`.`newCat`;
-
-DELIMITER $$
-USE `application`$$
-CREATE DEFINER=`root`@`%` PROCEDURE `newCat`(
-	in p_username varchar(45),
-	in p_description varchar(45)
-)
-begin
-	insert into category (username, description)
-    values (p_username, p_description);
-end$$
-
-DELIMITER ;
-
--- -----------------------------------------------------
--- procedure newUser
--- -----------------------------------------------------
-DROP procedure IF EXISTS `application`.`newUser`;
-
-DELIMITER $$
-USE `application`$$
-CREATE DEFINER=`root`@`127.0.0.1` PROCEDURE `newUser`(
-in p_username varchar(50),
-    in p_password varchar(255),
-    in p_email varchar(245)
-)
-begin
-
-    insert into user (username, password, email)
-    values (p_username, p_password, p_email);
-end$$
-
-DELIMITER ;
-
--- -----------------------------------------------------
--- procedure showCat
--- -----------------------------------------------------
-DROP procedure IF EXISTS `application`.`showCat`;
-
-DELIMITER $$
-USE `application`$$
-CREATE DEFINER=`root`@`%` PROCEDURE `showCat`()
-begin
-	select username, description
-    from category;
-end$$
-
-DELIMITER ;
-
--- -----------------------------------------------------
--- procedure updateProfil
--- -----------------------------------------------------
-DROP procedure IF EXISTS `application`.`updateProfil`;
-
-DELIMITER $$
-USE `application`$$
-CREATE DEFINER=`root`@`127.0.0.1` PROCEDURE `updateProfil`(
-in p_id int(11),
-in p_full_name varchar(255),
-    in p_picture blob
-)
-begin
-update user
-set
-full_name = p_full_name,
-        picture = p_picture
-where id = p_id;
-end$$
-
-DELIMITER ;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
