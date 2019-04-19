af22fa778c08dfdde4636580be73b9dcad2fa2da
Sindre Haavaldsen <sindre.h@outlook.com>
2019-04-19 19:55:19 +0200

    Kommentert notification

--- JS/notification.js
+++ JS/notification.js
@@ -1,4 +1,10 @@
 
+/*
+    Funksjon for meldingboks. Skrevet i ren JavaScript for å kunne laste inn i starten av alle sider uten å
+    laste inn hele jQuery biblioteket som kan gjøre innlasting av side tregere.
+    Kalles på med parameter for hva melding skal være. Bruker timer for timeout på når den skal
+    skjules av seg selv. Bruker kan også krysse ut ved trykk på tooltipExit som er krysset i boksen.    
+*/
 
 function TooltipMessage(text) { 
     var timer = 4000; 
