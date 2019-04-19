9fe9f2d52808f808f08b077370ef89ede9d4848c
Sindre Haavaldsen <sindre.h@outlook.com>
2019-04-19 19:55:54 +0200

    Kommentert profilePhoto

--- PHP/profilePhoto.php
+++ PHP/profilePhoto.php
@@ -12,7 +12,7 @@
             $location = '../Pictures/upload/';      
             if(move_uploaded_file($temp_name, $location.$name)){ //Hvis opplastning vellykket
                 $update = $link->query("UPDATE user SET avatar = '".$name."' WHERE id = $userId"); //Setter inn url for bildet
-                header("location: ../HTML/profil.php?upload=true"); // Vei rundt blank skjerm
+                header("location: ../HTML/profil.php?upload=true"); // Sender inn php variabel til side som aktiverer meldingboks om at opplastning var vellykket
             }
         }
         else {
