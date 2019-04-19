7f514c53f691e631598c5dcd7da166573f97c973
Sindre Haavaldsen <sindre.h@outlook.com>
2019-04-19 21:21:41 +0200

    Kommentert profilePhoto

--- PHP/profilePhoto.php
+++ PHP/profilePhoto.php
@@ -1,24 +1,17 @@
 <?php
     //Inkluder config fil for databasen
     include_once 'config.php';
+    
     //Konfigurer filopplastning
-  
-    $userId = $_SESSION['id'];
+    $userId = $_SESSION['id']; //Henter bruker id fra session variabel
     $name       = "USER_$userId ProfilePhoto.png"; //Setter filnavn  
-    $temp_name  = $_FILES['avatar']['tmp_name']; 
+    $temp_name  = $_FILES['avatar']['tmp_name']; //Setter opp temp navn som brukes til opplastning av fil
 
-    if(isset($name)){
-        if(!empty($name)){      
-            $location = '../Pictures/upload/';      
-            if(move_uploaded_file($temp_name, $location.$name)){ //Hvis opplastning vellykket
-                $update = $link->query("UPDATE user SET avatar = '".$name."' WHERE id = $userId"); //Setter inn url for bildet
-                header("location: ../HTML/profil.php?upload=true"); // Sender inn php variabel til side som aktiverer meldingboks om at opplastning var vellykket
-            }
-        }
-        else {
-            exit;
-        }       
-    }  else {
-        exit;
-    }
+    if(isset($name)){      
+        $location = '../Pictures/upload/';      
+        if(move_uploaded_file($temp_name, $location.$name)){  //Hvis opplastning vellykket
+            $update = $link->query("UPDATE user SET avatar = '".$name."' WHERE id = $userId"); //Setter inn url for bildet
+            header("location: ../HTML/profil.php?upload=true"); // Sender inn php variabel til side som aktiverer meldingboks om at opplastning var vellykket
+        } else { exit; }
+    } else { exit; }
 ?>
